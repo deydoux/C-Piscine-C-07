39e08a67d8dc32d141e4420a3e68ed31b3c616e9
Dorian Eydoux <deydoux@student.42lyon.fr>
2023-07-13 17:16:05 +0200

    Add ex01

---
+++ ex01/ft_range.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_range.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/13 09:44:13 by deydoux           #+#    #+#             */
+/*   Updated: 2023/07/13 16:42:46 by deydoux          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	*ft_range(int min, int max)
+{
+	int	i;
+	int	*range;
+
+	if (min >= max)
+		return (0);
+	range = malloc(sizeof(int) * (max - min));
+	if (!range)
+		return (NULL);
+	i = 0;
+	while (min < max)
+		range[i++] = min++;
+	return (range);
+}
