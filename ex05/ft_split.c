b8555b7af6ce93c990b14a9722dca281716ff536
Dorian Eydoux <dorian.eydoux@icloud.com>
2023-07-18 00:05:59 +0200

    Fix ex05

--- ex05/ft_split.c
+++ ex05/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/14 08:08:55 by deydoux           #+#    #+#             */
-/*   Updated: 2023/07/16 07:53:47 by deydoux          ###   ########.fr       */
+/*   Updated: 2023/07/18 00:05:36 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,79 +23,58 @@ int	contains(char *str, char c)
 	return (0);
 }
 
-char	**init_strs(char *str, char *charset)
+int	strs_size(char *str, char *charset)
 {
-	int		i;
-	int		size;
-	char	**strs;
+	int	i;
+	int	size;
 
-	if (!str[0])
-		return (NULL);
-	i = 1;
+	i = 0;
 	size = 1;
-	while (str[i])
+	while (str[i] != 0)
 	{
-		if (contains(charset, str[i]) && !contains(charset, str[i - 1])
-			&& str[i + 1] != 0)
-			size++;
+		if (!contains(charset, str[i]))
+			if (!str[i + 1] || contains(charset, str[i + 1]))
+				size++;
 		i++;
 	}
-	strs = malloc(sizeof(char *) * size + 1);
-	if (!strs)
-		return (NULL);
-	strs[size] = 0;
-	return (strs);
+	return (size);
 }
 
-char	*segment(char *src, char *charset, int *i)
+char	*segment_str(char *str, int start, int end)
 {
-	int		size;
-	char	*str;
-	int		j;
+	char	*segment;
+	int		i;
 
-	while (contains(charset, src[*i]))
-		(*i)++;
-	size = 0;
-	while (src[*i + size] && !contains(charset, src[*i + size]))
-		size++;
-	str = malloc(sizeof(char) * size + 1);
-	if (!str)
-		return (NULL);
-	j = 0;
-	while (str[j])
-		str[j++] = src[(*i)++];
-	str[size - 1] = 0;
-	return (str);
+	segment = malloc(sizeof(char) * (end - start + 1));
+	i = 0;
+	while (start + i <= end)
+	{
+		segment[i] = str[start + i];
+		i++;
+	}
+	segment[i] = 0;
+	return (segment);
 }
 
 char	**ft_split(char *str, char *charset)
 {
 	char	**strs;
+	int		start;
+	int		end;
 	int		i;
-	int		j;
 
-	strs = init_strs(str, charset);
-	if (!strs)
-		return (NULL);
+	strs = malloc(sizeof(char *) * strs_size(str, charset));
+	start = 0;
+	end = -1;
 	i = 0;
-	j = 0;
-	while (strs[i])
-		strs[i++] = segment(str, charset, &j);
+	while (str[++end])
+	{
+		if (!str[end] || contains(charset, str[end]))
+			start = end + 1;
+		if (!contains(charset, str[end])
+			&& (!str[end + 1] || contains(charset, str[end + 1])))
+			strs[i++] = segment_str(str, start, end);
+	}
+	strs[i] = 0;
 	return (strs);
-}
-
-void	ft_putstr(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i])
-		write(1, &str[i++], 1);
-}
-
-int	main(void)
-{
-	char	**strs = ft_split("Hello, World !", " ");
-
-	ft_putstr(strs[1]);
 }
